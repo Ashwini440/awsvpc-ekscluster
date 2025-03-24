29feacb201adb2e3b57964232d6dc4a6843ca3f5
Ashwini440 <140236944+Ashwini440@users.noreply.github.com>
2025-03-24 16:37:02 +0530

    Create variables.tf

---
+++ modules/eks-cluster/variables.tf
@@ -0,0 +1,24 @@
+variable "aws_region" {
+  type = string
+}
+variable "aws_vpc" {
+  type = string
+}
+variable "eks_cluster_role" {
+  type = string
+}
+variable "eks_cluster_name" {
+  type = string
+}
+variable "eks_node_role" {
+  type = string
+}
+variable "eks_nodegroup_name" {
+  type = string
+}
+variable "access_key" {
+  type        = string
+ }
+ variable "secret_access_key" {
+   type        = string
+ } 
